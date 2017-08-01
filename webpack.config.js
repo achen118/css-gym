85949acd7fdd22fa310033086b94c271b7071706
Alice Chen <alicechen118@gmail.com>
2017-07-31 21:41:52 -0700

    Style more, add basic level 1 rendering

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,13 @@
-const path = require("path");
-const webpack = require("webpack");
-
-let plugins = []; // if using any plugins for both dev and production
-let devPlugins = []; // if using any plugins for development
-
-let prodPlugins = [
-  new webpack.DefinePlugin({
-    'process.env': {
-      'NODE_ENV': JSON.stringify('production')
-    }
-  }),
-  new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: true
-    }
-  })
-];
-
-plugins = plugins.concat(
-  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
-);
-
 module.exports = {
-  context: __dirname,
-  entry: "./lib/entry.js",
+  entry: './lib/app.js',
   output: {
-    path: path.resolve(__dirname, "lib"),
-    filename: "bundle.js"
+    filename: './lib/bundle.js',
   },
-  plugins: plugins,
   module: {
     loaders: [
       {
-        test: /\.js?$/,
-        exclude: /node_modules/,
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
         loader: 'babel-loader',
         query: {
           presets: ['es2015']
@@ -43,6 +17,6 @@ module.exports = {
   },
   devtool: 'source-map',
   resolve: {
-    extensions: [".js", "*"]
+    extensions: ['.js', '*']
   }
 };
